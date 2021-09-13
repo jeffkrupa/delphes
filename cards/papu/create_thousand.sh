d32f4df0eecf547c0caf2421ebcffa807d53e51c
Horst Egon <benedikt.maier@cern.ch>
2021-09-13 13:35:11 -0400

    more cards

--- cards/papu/create_thousand.sh
+++ cards/papu/create_thousand.sh
@@ -1,1 +1,1 @@
-for i in {1001..2000}; do sed 's/XXX/'$i'/g' papu_CMS_PhaseII_HGCal.tcl > papu_CMS_PhaseII_HGCal_${i}.tcl; done
+for i in {8000..8100}; do sed 's/XXX/'$i'/g' papu_CMS_PhaseII_HGCal.tcl > papu_CMS_PhaseII_HGCal_${i}.tcl; done
