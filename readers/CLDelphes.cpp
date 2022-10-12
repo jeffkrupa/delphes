97320266420da730aaa1dec6bc97c0172b97f70d
Jeffrey Krupa <jeffrey.krupa@cern.ch>
2022-10-12 12:44:50 +0200

    add g->qq jettype

--- readers/CLDelphes.cpp
+++ readers/CLDelphes.cpp
@@ -275,7 +275,7 @@ int main(int argc, char *argv[])
       nparts = itree->GetLeaf("Particle_size")->GetValue(0);
 
       bool has_higgs = false;
-      /*TLorentzVector higgs(0.,0.,0.,0);
+      TLorentzVector higgs(0.,0.,0.,0);
       for (unsigned int w=0; w<nparts; w++){
 	
 	if (itree->GetLeaf("Particle.PID")->GetValue(w) == 25){
@@ -307,9 +307,56 @@ int main(int argc, char *argv[])
 	}
 
 	if ((tmp.DeltaR(higgs)<0.8) && (tmp.DeltaR(b1)<0.8) && (tmp.DeltaR(b2)<0.8)) {
-	  jettype = 4.;
+	  jettype = 6.;
 	}
-      } */
+      }
+      else{
+	TLorentzVector p1(0.,0.,0.,0);
+	TLorentzVector p2(0.,0.,0.,0);
+	p1.SetPtEtaPhiE(itree->GetLeaf("Particle.PT")->GetValue(1),itree->GetLeaf("Particle.Eta")->GetValue(1),itree->GetLeaf("Particle.Phi")->GetValue(1),itree->GetLeaf("Particle.E")->GetValue(1));
+	p2.SetPtEtaPhiE(itree->GetLeaf("Particle.PT")->GetValue(1),itree->GetLeaf("Particle.Eta")->GetValue(1),itree->GetLeaf("Particle.Phi")->GetValue(1),itree->GetLeaf("Particle.E")->GetValue(2));
+
+	//#### jettypes
+        //g
+	//uds
+	//g->qq
+	//c
+	//g->cc
+	//b
+	//g->bb
+	//H
+	//#####	
+	if ((tmp.DeltaR(p1)<0.8) && (tmp.DeltaR(p2)>0.8)){
+	  if (itree->GetLeaf("Particle.PID")->GetValue(1) == 21)
+	    jettype = 0.;
+	  else if (abs(itree->GetLeaf("Particle.PID")->GetValue(1)) <= 3)
+	    jettype = 1.;
+	  else if (abs(itree->GetLeaf("Particle.PID")->GetValue(1)) == 4)
+	    jettype = 3.;
+	  else if (abs(itree->GetLeaf("Particle.PID")->GetValue(1)) == 5)
+	    jettype = 5.;
+	}
+	else if ((tmp.DeltaR(p1)>0.8) && (tmp.DeltaR(p2)<0.8)){
+	  if (itree->GetLeaf("Particle.PID")->GetValue(2) == 21)
+	    jettype = 0.;
+	  else if (abs(itree->GetLeaf("Particle.PID")->GetValue(2)) <= 3)
+	    jettype = 1.;
+	  else if (abs(itree->GetLeaf("Particle.PID")->GetValue(2)) == 4)
+	    jettype = 3.;
+	  else if (abs(itree->GetLeaf("Particle.PID")->GetValue(2)) == 5)
+	    jettype = 5.;
+	}
+        else if ((tmp.DeltaR(p1)<0.8) && (tmp.DeltaR(p2)<0.8)){
+	  if ((abs(itree->GetLeaf("Particle.PID")->GetValue(1)) <= 3) && (abs(itree->GetLeaf("Particle.PID")->GetValue(2)) <= 3))
+	    jettype = 2.; //glightlight
+	  if ((abs(itree->GetLeaf("Particle.PID")->GetValue(1)) == 4) && (abs(itree->GetLeaf("Particle.PID")->GetValue(2)) == 4))
+	    jettype = 4.; //gcc
+	  if ((abs(itree->GetLeaf("Particle.PID")->GetValue(1)) == 5) && (abs(itree->GetLeaf("Particle.PID")->GetValue(2)) == 5))
+	    jettype = 6.; //gbb
+          
+        }
+      } 
+      /*
       TLorentzVector p1(0.,0.,0.,0);
       TLorentzVector p2(0.,0.,0.,0);
       TLorentzVector z(0.,0.,0.,0);
@@ -394,7 +441,7 @@ int main(int argc, char *argv[])
               jettype=5.;
 	  
       } 
-      
+      */ 
       
       if (jettype>-1.){
 	std::cout << jettype << std::endl;
