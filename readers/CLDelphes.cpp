f65c4724a568332bf918e32ec5a1fee786c99948
Jeffrey Krupa <jeffrey.krupa@cern.ch>
2023-03-20 15:02:11 +0100

    Fix CLDelphes

--- readers/CLDelphes.cpp
+++ readers/CLDelphes.cpp
@@ -8,7 +8,7 @@
 #include <time.h>
 #include <math.h>
 #include <numeric>
-
+#include <fstream>
 #include "TROOT.h"
 
 #include "TFile.h"
@@ -157,10 +157,6 @@ int main(int argc, char *argv[])
   TBranch* b_dau2_parton_e = tout->Branch("dau2_parton_e",&dau2_parton_e, "dau2_parton_e/F");
 
   TBranch* b_jettype = tout->Branch("jettype",&jettype, "jettype/F");
-  //TBranch* b_parton_pt = tout->Branch("parton_pt",&parton_pt, "parton_pt/F");
-  //TBranch* b_parton_eta = tout->Branch("parton_eta",&parton_eta, "parton_eta/F");
-  //TBranch* b_parton_phi = tout->Branch("parton_phi",&parton_phi, "parton_phi/F");
-  //TBranch* b_parton_e = tout->Branch("parton_e",&parton_e, "parton_e/F");
   TBranch* b_jet_pt = tout->Branch("jet_pt",&jet_pt, "jet_pt/F");
   TBranch* b_jet_eta = tout->Branch("jet_eta",&jet_eta, "jet_eta/F");
   TBranch* b_jet_phi = tout->Branch("jet_phi",&jet_phi, "jet_phi/F");
@@ -193,8 +189,12 @@ int main(int argc, char *argv[])
   fastjet::contrib::SoftDrop softDrop = fastjet::contrib::SoftDrop(sdBeta,sdZcut,radius);
   EnergyCorrelatorNseries N2fj(2,1.0,EnergyCorrelator::pt_R);
 
+  //std::ofstream outfile;
+  //outfile.open("../../jettype.txt", std::ios_base::trunc);
+
   for (unsigned int k=0; k<nevt; k++){
     itree->GetEntry(k);
+    //if( k>2000) break;
     if (k%100==0)
       std::cout << k << " / " << nevt << std::endl;
 
@@ -306,7 +306,7 @@ int main(int argc, char *argv[])
 	  }
 	}
 
-	if ((tmp.DeltaR(higgs)<0.8) && (tmp.DeltaR(b1)<0.8) && (tmp.DeltaR(b2)<0.8)) {
+	if ((tmp.DeltaR(higgs)<0.5) && (tmp.DeltaR(b1)<0.8) && (tmp.DeltaR(b2)<0.8)) {
 	  jettype = 4.;
 	}
       }
@@ -326,16 +326,16 @@ int main(int argc, char *argv[])
 	//g->cc
 	//g->bb
 	//g->gg
-	//#####	      
+	//#####	
 	TLorentzVector m(0.,0.,0.,0.);
         m = p1+p2;
         m_parton_pt = m.Pt();
         m_parton_eta = m.Eta();
         m_parton_phi = m.Phi();
         m_parton_e = m.E();
-	    
-	        
-	if ((tmp.DeltaR(p1)<0.8) && (tmp.DeltaR(p2)>0.8)){
+        
+
+	/*if ((tmp.DeltaR(p1)<0.8) && (tmp.DeltaR(p2)>0.8)){
 	  if (itree->GetLeaf("Particle.PID")->GetValue(1) == 21)
 	    jettype = 0.;
 	  else if (abs(itree->GetLeaf("Particle.PID")->GetValue(1)) <= 3)
@@ -349,7 +349,7 @@ int main(int argc, char *argv[])
           dau1_parton_phi = p1.Phi();
           dau1_parton_e = p1.E();
 	}
-	else if ((tmp.DeltaR(p1)>0.8) && (tmp.DeltaR(p2)<0.8)){
+	if ((tmp.DeltaR(p1)>0.8) && (tmp.DeltaR(p2)<0.8)){
 	  if (itree->GetLeaf("Particle.PID")->GetValue(2) == 21)
 	    jettype = 0.;
 	  else if (abs(itree->GetLeaf("Particle.PID")->GetValue(2)) <= 3)
@@ -362,8 +362,8 @@ int main(int argc, char *argv[])
           dau2_parton_eta = p2.Eta();
           dau2_parton_phi = p2.Phi();
           dau2_parton_e = p2.E();
-	}
-        else if ((tmp.DeltaR(p1)<0.8) && (tmp.DeltaR(p2)<0.8)){
+	}*/
+        if ((tmp.DeltaR(m)<0.5) && (tmp.DeltaR(p1)<0.8) && (tmp.DeltaR(p2)<0.8)){
 	  if ((abs(itree->GetLeaf("Particle.PID")->GetValue(1)) <= 3) && (abs(itree->GetLeaf("Particle.PID")->GetValue(2)) <= 3))
 	    jettype = 5.; //glightlight
 	  if ((abs(itree->GetLeaf("Particle.PID")->GetValue(1)) == 4) && (abs(itree->GetLeaf("Particle.PID")->GetValue(2)) == 4))
@@ -371,8 +371,7 @@ int main(int argc, char *argv[])
 	  if ((abs(itree->GetLeaf("Particle.PID")->GetValue(1)) == 5) && (abs(itree->GetLeaf("Particle.PID")->GetValue(2)) == 5))
 	    jettype = 7.; //gbb
 	  if ((abs(itree->GetLeaf("Particle.PID")->GetValue(1)) == 21) && (abs(itree->GetLeaf("Particle.PID")->GetValue(2)) == 21))
-	    jettype = 8.; //ggg
-
+            jettype = 8.; //ggg
       	  dau1_parton_pt = p1.Pt();
           dau1_parton_eta = p1.Eta();
           dau1_parton_phi = p1.Phi();
@@ -384,8 +383,97 @@ int main(int argc, char *argv[])
           dau2_parton_e = p2.E();
           
         }
+      } 
+      /*
+      TLorentzVector p1(0.,0.,0.,0);
+      TLorentzVector p2(0.,0.,0.,0);
+      TLorentzVector z(0.,0.,0.,0);
+      for (unsigned int w=0; w<nparts; w++){
+ 
+        if (itree->GetLeaf("Particle.PID")->GetValue(w) == 23){
+          z.SetPtEtaPhiM(itree->GetLeaf("Particle.PT")->GetValue(w),itree->GetLeaf("Particle.Eta")->GetValue(w),itree->GetLeaf("Particle.Phi")->GetValue(w),91);
+          break;
+        
+        }
       }
+      int mother_Idx = -1;
+      int dau1_Idx = -1;
+      int dau2_Idx = -1;
+      int m_pdgId = -99;
+      bool last_copy = false;
+      int first_copy = -1;
+      for (unsigned int w=0; w<nparts; w++){
+        
+        double dPt = TMath::Abs(itree->GetLeaf("Particle.PT")->GetValue(w) - z.Pt());
+        double dPhi = deltaPhi(itree->GetLeaf("Particle.Phi")->GetValue(w), z.Phi());
+        if ( (dPt<0.00001) && (dPhi - TMath::Pi() < 0.00001) && (itree->GetLeaf("Particle.PID")->GetValue(w) != 23) ){ //first parton with same pt and opposite phi as Z boson 
+          first_copy = w;
+          m_pdgId = itree->GetLeaf("Particle.PID")->GetValue(w);
+	  break;
+        } 
+
+      }
+      for (unsigned int w=first_copy; w<nparts; w++){
+        if( m_pdgId != itree->GetLeaf("Particle.PID")->GetValue(w)) continue; //only look at subsequent particles of same type 
+	if( itree->GetLeaf("Particle.D1")->GetValue(w) == itree->GetLeaf("Particle.D2")->GetValue(w)) continue; //if D1==D2 it's not the last copy
+	else{  //it is the last copy
+	  mother_Idx = w;
+	  dau1_Idx = itree->GetLeaf("Particle.D1")->GetValue(w);
+	  dau2_Idx = itree->GetLeaf("Particle.D2")->GetValue(w);
+	  break;
+	}
+      }
+
+      if (mother_Idx>-1){
+          m_parton_pt = itree->GetLeaf("Particle.PT")->GetValue(mother_Idx);
+          m_parton_eta = itree->GetLeaf("Particle.Eta")->GetValue(mother_Idx);
+          m_parton_phi = itree->GetLeaf("Particle.Phi")->GetValue(mother_Idx);
+          m_parton_e = itree->GetLeaf("Particle.E")->GetValue(mother_Idx);
+          if (dau1_Idx>-1){
+              p1.SetPtEtaPhiE(itree->GetLeaf("Particle.PT")->GetValue(dau1_Idx),itree->GetLeaf("Particle.Eta")->GetValue(dau1_Idx),itree->GetLeaf("Particle.Phi")->GetValue(dau1_Idx),itree->GetLeaf("Particle.E")->GetValue(dau1_Idx));
+      	      dau1_parton_pt = p1.Pt();
+              dau1_parton_eta = p1.Eta();
+              dau1_parton_phi = p1.Phi();
+              dau1_parton_e = p1.E();
+	  }
+          if (dau2_Idx>-1){
+              p2.SetPtEtaPhiE(itree->GetLeaf("Particle.PT")->GetValue(dau2_Idx),itree->GetLeaf("Particle.Eta")->GetValue(dau2_Idx),itree->GetLeaf("Particle.Phi")->GetValue(dau2_Idx),itree->GetLeaf("Particle.E")->GetValue(dau2_Idx));
+              dau2_parton_pt = p2.Pt();
+              dau2_parton_eta = p2.Eta();
+              dau2_parton_phi = p2.Phi();
+              dau2_parton_e = p2.E();
+	  }
+          //std::cout <<"mother " << itree->GetLeaf("Particle.PID")->GetValue(mother_Idx)<<"\tdau1 " << itree->GetLeaf("Particle.PID")->GetValue(dau1_Idx) << "\tdau2 " << itree->GetLeaf("Particle.PID")->GetValue(dau2_Idx) << std::endl;
+          if (dau1_Idx == -1 || dau2_Idx==-1) continue; //?
+	  if ((tmp.DeltaR(p1)>0.8) && (tmp.DeltaR(p2)>0.8)) continue;
+
+          int dau1_pdgId, dau2_pdgId;
+          dau1_pdgId = itree->GetLeaf("Particle.PID")->GetValue(dau1_Idx);
+          dau2_pdgId = itree->GetLeaf("Particle.PID")->GetValue(dau2_Idx);
+
+	  if (m_pdgId==21){
+               
+     	      if ((abs(dau1_pdgId) == 4) && (abs(dau2_pdgId) == 4)){
+                  jettype=1.;
+	      } 
+     	      else if ((abs(dau1_pdgId) == 5) && (abs(dau2_pdgId) == 5)){
+                  jettype=2.; 
+	      }
+              else jettype=0.; 
+          }
+	  else if (abs(m_pdgId)<=3)
+              jettype=3.;
+    	  else if (abs(m_pdgId)==4)
+              jettype=4.;
+    	  else if (abs(m_pdgId)==5)
+              jettype=5.;
+	  
+      } 
+      */ 
+      
       if (jettype>-1.){
+	//std::cout << jettype << std::endl;
+        //outfile << jettype << "\n";
 
 	fastjet::PseudoJet sdJet = (softDrop)(jet);
 
@@ -420,9 +508,8 @@ int main(int argc, char *argv[])
 	tout->Fill();
 
       }
-
+      if (dau1_parton_pt > 0.) break;
     }
-
     //tout->Fill();
 
     progressBar.Update(k, k);
